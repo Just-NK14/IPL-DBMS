b2e59d724e2d8cb0aa85f88208fb07e42f1904f2
Navaneeth Krishna <144162909+Just-NK14@users.noreply.github.com>
2024-05-03 10:52:52 +0530

    Update logd.sql

--- logd.sql
+++ logd.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3307
--- Generation Time: Apr 27, 2024 at 09:30 PM
+-- Generation Time: May 03, 2024 at 07:22 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -29,7 +29,6 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `login` (
   `username` varchar(30) NOT NULL,
-  `password` varchar(30) NOT NULL,
   `hashed_password` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
@@ -37,11 +36,21 @@ CREATE TABLE `login` (
 -- Dumping data for table `login`
 --
 
-INSERT INTO `login` (`username`, `password`, `hashed_password`) VALUES
-('ipl', 'admin', '$2y$10$2YxmhGT.wef/tGug.w1dF.TeHUQm9EzufeDM1i.1jus/xCYOA2NZy'),
-('ipl1', 'admin1', '$2y$10$JVB8ERrn6hDVhb0eNgWpmuCD44TFpuM9W.y5jM0CDTFD26kyj1vd6'),
-('navaneeth', 'krishna', '$2y$10$wxDarb/yqBr0q1aYxaTNk.glBNlnQRVVhfyi5VPKGaYxYgXX5KuYm'),
-('sai', 'kiran', '$2y$10$33xjpLzinh4YpJeFsz9xresMPblpc91Fz4uVuefj28rGq3XBeT0yO');
+INSERT INTO `login` (`username`, `hashed_password`) VALUES
+('ipl', '$2y$10$2YxmhGT.wef/tGug.w1dF.TeHUQm9EzufeDM1i.1jus/xCYOA2NZy'),
+('ipl1', '$2y$10$JVB8ERrn6hDVhb0eNgWpmuCD44TFpuM9W.y5jM0CDTFD26kyj1vd6'),
+('navaneeth', '$2y$10$wxDarb/yqBr0q1aYxaTNk.glBNlnQRVVhfyi5VPKGaYxYgXX5KuYm'),
+('sai', '$2y$10$33xjpLzinh4YpJeFsz9xresMPblpc91Fz4uVuefj28rGq3XBeT0yO');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `login`
+--
+ALTER TABLE `login`
+  ADD PRIMARY KEY (`username`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
